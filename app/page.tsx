b94a34587b9d3bbc3be456e8bb66bc07c36839df
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-05-23 14:27:43 +0200

    test frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 "use client"
-import ProductList from "@/components/ProductList"
-import ShopInfo from "@/components/ShopInfo"
 
 const Home = () => {
   return (
     <div className="max-w-6xl mx-auto p-4 h-screen">
-      <ShopInfo />
-      <ProductList />
+      <h1> WELCOME </h1>
     </div>
   )
 }
